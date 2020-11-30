d53e240f85706c23e59d2892e601905095fb9370
Benjamin Jakubowski <buj201@crackle2.cims.nyu.edu>
2020-11-30 12:45:51 -0500

    Remove existing RepCRec.rpz.

--- make_reprozip.sh
+++ make_reprozip.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+rm RepCRec.rpz
 reprozip trace --overwrite ./run_custom_tests.sh
 reprozip trace --continue python3 -m src.transaction_manager tests/provided_tests/test1.txt
 reprozip pack RepCRec.rpz
